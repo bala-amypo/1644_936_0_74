e9f353acffe8eeed1a51a39b2ecf91da7512c92e
bala-amypo <bala@amypo.in>
2025-12-18 08:54:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/Validationcontroller.java
+++ src/main/java/com/example/demo/controller/Validationcontroller.java
@@ -2,12 +2,11 @@ package com.example.demo.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.DeleteMapping;
+
+
 
 
 import com.example.demo.entity.Validationentity;
@@ -20,8 +19,8 @@ public class Validationcontroller{
    @Autowired Validationservice ser;
 
    @PostMapping("/post")
-   public Validationentity sendData(@RequestBody Validationentity stu){
-    return ser.postData(stu);
+   public Validationentity sendData(@valid @RequestBody Validationentity entity){
+    return ser.postData(entity);
    }
    
 
--- src/main/java/com/example/demo/service/Validationserviceimpl.java
+++ src/main/java/com/example/demo/service/Validationserviceimpl.java
@@ -2,9 +2,9 @@ package com.example.demo.service.Impl;
 
 import org.springframework.stereotype.Service;
 import com.example.demo.service.Validationtservice;
-import com.example.demo.repository.Studentrepo;
+import com.example.demo.repository.Validationrepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.example.demo.entity.Studententity;
+import com.example.demo.entity.Validationentity;
 
 
 @Service
