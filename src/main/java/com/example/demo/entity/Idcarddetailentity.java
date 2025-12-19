02e3aa7a64bd426bad5041472b63f2961c74638f
bala-amypo <bala@amypo.in>
2025-12-19 09:49:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/Idcarddetailentity.java
+++ src/main/java/com/example/demo/entity/Idcarddetailentity.java
@@ -1,9 +1,21 @@
 package com.example.demo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Idcarddetailentity{
-
+     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
     private String idcardnumber;
     private String studentid;
