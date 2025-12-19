3f9292ea263ceeea6ad958149b77627d00993941
bala-amypo <bala@amypo.in>
2025-12-19 10:30:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/Idcarddetailserviceimpl.java
@@ -0,0 +1,18 @@
+package com.example.demo.service.Impl;
+
+import org.springframework.stereotype.Service;
+import com.example.demo.service.Studentservice;
+import com.example.demo.repository.Studentrepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.entity.Studententity;
+
+import java.util.List;
+
+@Service
+public class Studentserviceimpl implements Studentservice{
+     @Autowired Studentrepo student;
+    
+     @Override
+    public Studententity postData(Studententity stu){
+        return student.save(stu);
+    }
---
+++ src/main/java/com/example/demo/service/Impl/Studentdetailsserviceimpl.java
@@ -0,0 +1,18 @@
+package com.example.demo.service.Impl;
+
+import org.springframework.stereotype.Service;
+import com.example.demo.service.Studentdetailsservice;
+import com.example.demo.repository.Studendetailstrepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.entity.Studentdetailsentity;
+
+
+
+@Service
+public class Studentdetailsserviceimpl implements Studentdetailsservice{
+     @Autowired Studentdetailsrepo student;
+    
+     @Override
+    public Studentdetailsentity postData(Studentdetailsentity stu){
+        return student.save(stu);
+    }
